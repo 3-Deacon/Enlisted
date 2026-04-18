ccc1e6d24980f81d687c9c3e8479d6f82f558b46
Arkpointt <kyle@arkpoint.tech>
2026-04-18 14:49:42 -0500

    feat: add StoryTier and StoryBeat enums for pacing director

---
+++ src/Features/Content/StoryBeat.cs
@@ -0,0 +1,26 @@
+namespace Enlisted.Features.Content
+{
+    public enum StoryBeat
+    {
+        LordCaptured,
+        LordKilled,
+        LordMajorBattleStart,
+        LordMajorBattleEnd,
+        PlayerBattleEnd,
+        SiegeBegin,
+        SiegeEnd,
+        WarDeclared,
+        PeaceSigned,
+        ArmyFormed,
+        ArmyDispersed,
+        SettlementEntered,
+        SettlementLeft,
+        PaydayBeat,
+        OrderPhaseTransition,
+        OrderComplete,
+        CompanyCrisisThreshold,
+        CompanyPressureThreshold,
+        EscalationThreshold,
+        QuietStretchTimeout
+    }
+}
---
+++ src/Features/Content/StoryTier.cs
@@ -0,0 +1,10 @@
+namespace Enlisted.Features.Content
+{
+    public enum StoryTier
+    {
+        Log = 0,
+        Pertinent = 1,
+        Headline = 2,
+        Modal = 3
+    }
+}
